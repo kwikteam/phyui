a04d7ea036e910fa440bd05d93e8137bdcaad600
Cedric GESTES <cedric.gestes@gmail.com>
2015-04-15 17:50:45 +0200

    kernel_call: handle special % call.
    (in fact reply is a better way to check for the status but it doesnt include the output..)

--- phyui/static/notebook/ipython_kernel_call.js
+++ phyui/static/notebook/ipython_kernel_call.js
@@ -14,6 +14,23 @@ define(function(require) {
     }
 
     var _handle_reply = function(success_cb, error_cb, msg) {
+      if (msg.msg_type != "execute_reply") {
+        console.log("ipython.call unhandled message type:", msg.msg_type);
+        return;
+      }
+      if (msg.content.status == "error") {
+        if (error_cb)
+          error_cb(msg);
+        else
+          console.log("ipython.call error:", msg.content.ename, ":", msg.content.evalue);
+        }
+      else if (msg.content.status == "ok") {
+        if (success_cb)
+          success_cb(msg);
+        }
+    }
+
+    var _handle_reply_output = function(success_cb, error_cb, msg) {
         if (msg.msg_type == "error") {
             if (error_cb)
                 error_cb(msg);
@@ -34,21 +51,32 @@ define(function(require) {
     //   success : msg.content.data
     //   error   : msg.content.{ename,evalue}
     // kernel is optional
+    // special is to be use for % command. the return value is not given the same way.
     var ipython_call = function(code, success_cb, error_cb, kernel) {
         var cbs = {
             shell : {
-                reply : dolog.bind(undefined, 'reply'),
+                reply : _handle_reply.bind(undefined, success_cb, error_cb),
                 payload : {
                     set_next_input : dolog.bind(undefined, 'set_next_input'),
                     page : dolog.bind(undefined, 'page'),
                 }
             },
             iopub : {
-                output : _handle_reply.bind(undefined, success_cb, error_cb),
+                output : _handle_reply_output.bind(undefined, success_cb, error_cb),
                 clear_output : dolog.bind(undefined, 'clear_output'),
             },
             //input : dolog.bind(undefined, 'input'),
         };
+
+        //special case for special % function.
+        //we use the reply status. (which do not contain the data)
+        //no way to have the return value and the correct reply status...
+        if (code.indexOf("%") == 0) {
+          cbs.iopub.output = undefined;
+        } else {
+          cbs.shell.reply = undefined;
+        }
+
         try {
           if (!kernel)
             kernel = IPython.notebook.session.kernel;
